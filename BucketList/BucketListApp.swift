692a34583b59cde0a1572f244861cd0d3ed57c73
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-06-09 19:18:20 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Godwin IE on 09/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
