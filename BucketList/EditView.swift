077ffb0d46f92578ac898222721457cee7cc251e
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-07-16 13:39:48 +0100

    network call and displaying nearby places

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -42,7 +42,7 @@ struct EditView: View {
                             
                             + Text(": ") +
                             
-                            Text("Page description here")
+                            Text(page.description)
                                 .italic()
                         }
                         
@@ -64,6 +64,9 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         } //nav stack
     }
     
@@ -73,6 +76,24 @@ struct EditView: View {
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("bad url: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
     }
 }
 
